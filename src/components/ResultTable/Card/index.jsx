08cf10e5a7a164ce188e40253d9d9cae1e6bcdc8
fponce99 <fponce@espol.edu.ec>
2023-10-15 23:24:46 -0500

    store-6: Modal and loader

--- src/components/ResultTable/Card/index.jsx
+++ src/components/ResultTable/Card/index.jsx
@@ -1,15 +1,31 @@
-
+import { useContext } from 'react';
 import { Detail } from './Detail'
+import { SearchContext } from '../../../contexts/SearchContext';
 import './Card.css'
 
-function Card ({ image, title, price } ) {
+function Card ({ image, title, price, description }) {
+    const {
+        setIsOpen,
+        setImageProduct,
+        setTitleProduct,
+        setPriceProduct,
+        setDescriptionProduct,
+    } = useContext(SearchContext);
+
+    const openModal = () => {
+        setIsOpen(true)
+        setImageProduct(image)
+        setTitleProduct(title)
+        setPriceProduct(price)
+        setDescriptionProduct(description)
+    }
+
     return (
-        <div className='CardContainer'>
+        <div className='CardContainer' onClick={openModal}>
             <div className='ProductImageContainer'>
                 <img src={image}/>
             </div>
             <Detail 
-                image = {image}
                 title = {title}
                 price = {price}
             />
